95d254f3d826c209a0a4171d21da5f035a3bea50
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-12-06 20:54:54 -0600

    Added Firebase storage Cocoapod and now can upload image to firebase

--- Snapchat/Snapchat/controller/SendSnapViewController.swift
+++ Snapchat/Snapchat/controller/SendSnapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class SendSnapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
@@ -26,12 +27,34 @@ class SendSnapViewController: UIViewController, UIImagePickerControllerDelegate,
     //MARK: Actions
     @IBAction func cameraTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func myPhotosTapped(_ sender: Any) {
         imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func nextTapped(_ sender: Any) {
+        let imageFolder = Storage.storage().reference().child("images")
+        
+        if let image = snapImageView.image {
+            if let imageData = UIImageJPEGRepresentation(image, 0.1) {
+                imageFolder.child("mySnap.jpeg").putData(imageData, metadata: nil, completion: { (meta, error) in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        print("Success uploading image")
+                    }
+                })
+            }
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            snapImageView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 }
